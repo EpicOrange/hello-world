e706438d0e0ad18e34c90fd39cdea892413a3543
EpicOrange <danieldoesntcheckthis@gmail.com>
2014-07-16 22:05:29 -0700

    Update main.cpp
    oh vi is way better than this here

--- main.cpp
+++ main.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 
-using namespace std;
-
 int main() {
-	cout << "what am i doing" << endl;
+	std::cout << "what am i doing" << std::endl;
 	
+	std::string in;
+	std::cin >> in;
 	
+	std::cout << "no, you are doing magic." << std::endl;
+	std::cout << "lots of magic." << std::endl;
+	std::cout << "..." << std::endl;
 	
+	std::cin >> in;
 	
-	
-	
-	
-	
+	std::cout << "please stop. it's disrupting the leylines and you don't have authorization anyway" << std::endl;
 	
 	return 0;
 }
